f676814e08306b0206c39d8bb058ed21ed6afe82
Jihyun Yu <yjh0502@gmail.com>
2020-08-31 21:47:12 +0900

    ignore_whitespace/case_insensitive

--- src/filter.rs
+++ src/filter.rs
@@ -5,6 +5,8 @@ use wasm_bindgen::prelude::*;
 #[derive(Clone, Debug)]
 pub struct Substring<'a> {
     subseq: &'a [u8],
+    pub ignore_whitespace: bool,
+    pub case_insensitive: bool,
 }
 
 impl<'a> Substring<'a> {
@@ -14,15 +16,17 @@ impl<'a> Substring<'a> {
     pub fn new(subsequence: &'a str) -> Self {
         Self {
             subseq: subsequence.as_bytes(),
+            ignore_whitespace: false,
+            case_insensitive: false,
         }
     }
 }
 
-#[derive(PartialEq, Eq, Clone, Copy)]
+#[derive(Clone)]
 pub enum SubstringState {
     Index(usize),
     NotMatched,
-    Matched,
+    Matched(usize),
 }
 
 impl<'a> Automaton for Substring<'a> {
@@ -35,14 +39,18 @@ impl<'a> Automaton for Substring<'a> {
 
     #[inline]
     fn is_match(&self, state: &Self::State) -> bool {
-        state == &SubstringState::Matched
+        if let SubstringState::Matched(_) = state {
+            true
+        } else {
+            false
+        }
     }
 
     #[inline]
     fn can_match(&self, state: &Self::State) -> bool {
         match state {
             SubstringState::Index(_) => true,
-            SubstringState::Matched => true,
+            SubstringState::Matched(_) => true,
             SubstringState::NotMatched => false,
         }
     }
@@ -51,7 +59,7 @@ impl<'a> Automaton for Substring<'a> {
     fn will_always_match(&self, state: &Self::State) -> bool {
         match state {
             SubstringState::Index(_) => false,
-            SubstringState::Matched => true,
+            SubstringState::Matched(_) => true,
             SubstringState::NotMatched => false,
         }
     }
@@ -60,25 +68,37 @@ impl<'a> Automaton for Substring<'a> {
     fn accept(&self, state: &Self::State, byte: u8) -> Self::State {
         match state {
             SubstringState::Index(idx) => {
-                if byte == self.subseq[*idx] {
-                    if *idx == self.subseq.len() - 1 {
-                        SubstringState::Index(*idx)
-                    } else {
-                        SubstringState::Index(*idx + 1)
+                let mut idx = *idx;
+                if self.ignore_whitespace {
+                    while idx < self.subseq.len() && self.subseq[idx].is_ascii_whitespace() {
+                        idx += 1;
                     }
+                }
+                if idx == self.subseq.len() {
+                    SubstringState::Index(idx)
                 } else {
-                    SubstringState::NotMatched
+                    let matched = if self.case_insensitive {
+                        byte.to_ascii_lowercase() == self.subseq[idx].to_ascii_lowercase()
+                    } else {
+                        byte == self.subseq[idx]
+                    };
+
+                    if matched {
+                        SubstringState::Index(idx + 1)
+                    } else {
+                        SubstringState::NotMatched
+                    }
                 }
             }
-            other => *other,
+            other => other.clone(),
         }
     }
 
     #[inline]
     fn accept_eof(&self, state: &Self::State) -> Option<Self::State> {
         match state {
-            SubstringState::Index(_) => Some(SubstringState::Matched),
-            SubstringState::Matched => Some(SubstringState::Matched),
+            SubstringState::Index(idx) => Some(SubstringState::Matched(*idx)),
+            SubstringState::Matched(idx) => Some(SubstringState::Matched(*idx)),
             SubstringState::NotMatched => None,
         }
     }
@@ -117,6 +137,15 @@ impl Filter {
     }
 
     pub fn filter(&self, s: &str) -> FilterResult {
+        self.filter_opts(s, false, false)
+    }
+
+    pub fn filter_opts(
+        &self,
+        s: &str,
+        ignore_whitespace: bool,
+        case_insensitive: bool,
+    ) -> FilterResult {
         use fst::Streamer;
 
         let mut out = String::new();
@@ -127,23 +156,33 @@ impl Filter {
             if last_idx > idx {
                 continue;
             }
+            if ch.is_ascii_whitespace() {
+                out.push(ch);
+                continue;
+            }
 
-            let substr = &s[idx..];
+            let mut subseq = Substring::new(&s[idx..]);
+            subseq.ignore_whitespace = ignore_whitespace;
+            subseq.case_insensitive = case_insensitive;
 
-            let subseq = Substring::new(substr);
-            let mut stream = self.inner.search(subseq).into_stream();
-            while let Some(m) = stream.next() {
-                let next_idx = idx + m.len();
-                if next_idx > last_idx {
-                    last_idx = next_idx;
+            let mut keyword = None;
+            let mut stream = self.inner.search_with_state(subseq).into_stream();
+            while let Some((_m, state)) = stream.next() {
+                if let SubstringState::Index(end_idx) = state {
+                    let next_idx = idx + end_idx;
+                    if next_idx > last_idx {
+                        last_idx = next_idx;
+                        keyword = std::str::from_utf8(_m).ok().map(|s| s.to_string());
+                    }
                 }
             }
 
             if last_idx > idx {
-                let keyword = &s[idx..last_idx];
-                keywords.push(keyword.to_owned());
-                for _ in keyword.char_indices() {
-                    out.push('*');
+                if let Some(keyword) = keyword {
+                    for _ in keyword.char_indices() {
+                        out.push('*');
+                    }
+                    keywords.push(keyword);
                 }
             } else {
                 out.push(ch);
@@ -178,6 +217,19 @@ mod tests {
         assert_eq!(
             filter.filter("foo bazbaz bar foof bar").result,
             "*** bazbaz *** ***f ***"
+        );
+    }
+
+    #[test]
+    fn whitespace() {
+        let keys = &["foo", "bar", "foofo", "한글"];
+
+        let filter = Filter::new(keys);
+
+        // longest match
+        assert_eq!(
+            filter.filter_opts("foo  B a\tr", true, true).result,
+            "***  ***"
         );
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,14 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn filter(s: &str, words: Box<[JsValue]>) -> JsValue {
+pub fn filter(
+    s: &str,
+    words: Box<[JsValue]>,
+    ignore_whitespace: bool,
+    case_insensitive: bool,
+) -> JsValue {
     let filter = Filter::from_js(words);
-    let filtered = filter.filter(s);
+    let filtered = filter.filter_opts(s, ignore_whitespace, case_insensitive);
     if let Ok(val) = JsValue::from_serde(&filtered) {
         val
     } else {
